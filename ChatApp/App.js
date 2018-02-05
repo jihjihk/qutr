2f90e37bd3f31c5795a4cce8f7fca7ddf3fa629c
Peter Chen <manchesterutd7@gmail.com>
2018-02-05 19:07:34 +0400

    added src folder

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -6,7 +6,9 @@
 
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+
 import { configureStore } from './src/store';
+
 import Chat from './src/components/Chat';
 
 const store = configureStore();
