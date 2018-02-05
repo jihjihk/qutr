db83d2bc34fb4ee6679424d71f7f720ffc71a134
Peter Chen <manchesterutd7@gmail.com>
2018-02-05 19:13:17 +0400

    added images

---
+++ ChatApp/src/configs/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    // Initialize Firebase
+      apiKey: "AIzaSyCFEAP7TgO1Z3WFdMj46IP11pv4YhnWLJE",
+      authDomain: "chatapp-a066a.firebaseapp.com",
+      databaseURL: "https://chatapp-a066a.firebaseio.com",
+      projectId: "chatapp-a066a",
+      storageBucket: "chatapp-a066a.appspot.com",
+      messagingSenderId: "99616541215"
+};
---
+++ ChatApp/src/i18n/locales/en.js
@@ -0,0 +1,17 @@
+export default {
+    login: 'Login',
+    signup: 'Sign Up',
+  
+    email: 'Email',
+    password: 'Password',
+  
+    chat: 'Chat',
+    message: 'Message',
+  
+    error: 'Error',
+  
+    you: 'You',
+  
+    placeholder: 'There are no messages yet',
+  }
+  
