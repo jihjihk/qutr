e81beed26afe2e7f92ae51ba82b07cde2e53bc0e
jihjihk <jihyun@nyu.edu>
2018-04-12 16:09:59 +0400

    chinese segemented json added

--- backend/db0328/csv_to_json.py
+++ backend/db0328/csv_to_json.py
@@ -8,13 +8,15 @@ def main(inputcsv):
 	en = pd.DataFrame(tri.en)
 	ar = pd.DataFrame(tri.ar)
 	cn = pd.DataFrame(tri.cn)
+	cn_split = pd.DataFrame(tri.cn_split)
 	pos = pd.DataFrame(tri.pos)
 
 	en = en.rename(columns={"en":"phrase"})
 	ar = ar.rename(columns={"ar":"phrase"})
 	cn = cn.rename(columns={"cn":"phrase"})
+	cn_split = cn_split.rename(columns={"cn_split":"phrase"})
 
-	for langdf in [en, ar, cn]:
+	for langdf in [en, ar, cn, cn_split]:
 	    langdf['pos'] = pos
 	    for i in range(0, len(langdf)):
 	        ## assigning a new index col with p0, p1... to differentiate string IDs with cardinal numbers
@@ -39,6 +41,12 @@ def main(inputcsv):
 
 	with open('cn.json', 'w') as outfile:
 	    json.dump(cnob, outfile)
+
+	cn_split_json = cn_split.to_json(orient='index')
+	cn_splitob = json.loads(cn_split_json)
+
+	with open('cn_split.json', 'w') as outfile:
+	    json.dump(cn_splitob, outfile)
 
 if __name__ == "__main__":
 	main(sys.argv[1])
